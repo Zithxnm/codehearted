69d19663cca7c4c77c8cefd290e434ebb74f72f2
Errol <errl.zth@gmail.com>
2025-11-14 14:20:25 +0800

    logout implementation

---
+++ app/Http/Controllers/Auth/LogoutController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
+
+class LogoutController extends Controller
+{
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        return redirect('/login');
+    }
+}
