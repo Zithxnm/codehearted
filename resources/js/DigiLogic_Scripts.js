28dd1b201413f13483438679ac13d4ce47cb31b1
Errol <errl.zth@gmail.com>
2025-12-03 01:11:21 +0800

    Fixes to quizzes and practice quizzes

--- resources/js/DigiLogic_Scripts.js
+++ resources/js/DigiLogic_Scripts.js
@@ -26,29 +26,5 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-// 3. Module Tab Switching Logic
-// We attach this to 'window' so your Blade onclick="selectModule(...)" can find it.
-window.selectModule = function(element, index) {
-    // A. Remove 'active' class from all sidebar items
-    document.querySelectorAll('.module-item').forEach(el => {
-        el.classList.remove('active');
-    });
-
-    // B. Add 'active' class to the clicked item
-    element.classList.add('active');
-
-    // C. Hide all lesson detail cards (Right Panel)
-    document.querySelectorAll('.lesson-card').forEach(card => {
-        card.style.display = 'none';
-        card.classList.remove('active');
-    });
-
-    // D. Show the specific card that matches the index
-    const targetCard = document.querySelector(`.lesson-card[data-module-index="${index}"]`);
-    if (targetCard) {
-        targetCard.style.display = 'block';
-        targetCard.classList.add('active');
-    }
-};
 
 
--- resources/views/php/Course_Show.blade.php
+++ resources/views/php/Course_Show.blade.php
@@ -168,7 +168,24 @@
                     <li><a href="{{ route('about') }}" class="footer-link"><span>➤ About Us</span></a></li>
                 </ul>
             </div>
+
+            <div class="footer-section">
+                <h4 class="footer-title">Stay sharp as a fox — follow us for news and updates.</h4>
+                <div class="social-links">
+                    <a href="https://www.facebook.com/PampangaStateU" class="social-link" aria-label="PSU" target="_blank">
+                        <img class="psu" src="{{ asset('imgs/WhiteLogo_PSU.png') }}" alt="PSU">
+                    </a>
+                    <a href="https://www.facebook.com/dhvsu.ccssc" class="social-link" aria-label="CCS" target="_blank">
+                        <img class="ccs" src="{{ asset('imgs/WhiteLogo_CCSSC.png') }}" alt="PSU">
+                    </a>
+                    <a href="https://www.facebook.com/ComPressCCS" class="social-link" aria-label="ComPress" target="_blank">
+                        <img class="compress" src="{{ asset('imgs/WhiteLogo_ComPress.png') }}" alt="PSU">
+                    </a>
+                </div>
+            </div>
         </div>
+
+
         <div class="footer-bottom">
             <p>© 2025 CodeHearted. All rights reserved.</p>
         </div>
